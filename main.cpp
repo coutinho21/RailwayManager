7eaca7b9ee907e4a18844b31469b0a4663dcd81c
coutinho21 <guilhermecscoutinho@gmail.com>
2023-03-19 14:49:23 +0000

    Added menus and options

--- main.cpp
+++ main.cpp
@@ -6,13 +6,18 @@ using namespace std;
 Graph railway;
 
 void showMenu() {
-    cout << "\n";
-    cout << "0) Exit\n";
+    cout << "\n0) Exit\n";
+    cout << "1) List station attributes\n";
+    cout << "2) Shortest path between two stations\n";
+    cout << "Choose an option: ";
+}
+
+void showListStationAttributes(){
+    cout << "\n0) Go back\n";
     cout << "1) Show all stations\n";
     cout << "2) Show all connections\n";
     cout << "3) Show all connections from a station\n";
     cout << "4) Show all connections to a station\n";
-    cout << "5) Show all connections between two stations\n";
     cout << "Choose an option: ";
 }
 
@@ -27,11 +32,67 @@ int readOption() {
     return option;
 }
 
+string readInput(){
+    string input;
+    getline(cin >> ws, input);
+    return input;
+}
+
+void listStationAttributes(){
+    int option;
+    string stationName, stationName2;
+    do {
+        showListStationAttributes();
+        option = readOption();
+        switch (option) {
+            case 0:
+                break;
+            case 1:
+                railway.printStations();
+                break;
+            case 2:
+                railway.printAllConnections();
+                break;
+            case 3:
+                cout << "\nStation name: ";
+                stationName = readInput();
+                railway.getStations()[stationName]->printConnections();
+                break;
+            case 4:
+                cout << "\nStation name: ";
+                stationName = readInput();
+                railway.printConnectionsTo(stationName);
+                break;
+            default:
+                cout << "\nInvalid option. Try again.\n";
+        }
+    } while (option != 0);
+}
+
 int main() {
     railway.readFiles("../data/stations.csv", "../data/network.csv");
-    //railway.printStations();
-    for(auto &station : railway.getStations()) {
-        station.second->printConnections();
-    }
+    int option;
+    string stationName, stationName2;
+    do {
+        showMenu();
+        option = readOption();
+        switch (option) {
+            case 0:
+                cout << "\nBye!\n";
+                break;
+            case 1:
+                listStationAttributes();
+                break;
+            case 2:
+                cout << "\nStation name: ";
+                stationName = readInput();
+                cout << "Station name: ";
+                stationName2 = readInput();
+                railway.dijkstra(stationName, stationName2);
+                break;
+            default:
+                cout << "\nInvalid option. Try again.\n";
+        }
+    } while (option != 0);
     return 0;
 }
